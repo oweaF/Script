84b1ef836c0ada303a8f00e10a25ae92a8bd029a
oweaF <oweafaltfeng97@gmail.com>
2020-01-14 10:31:38 +0800

    Delete remote unlock.js

--- unblockremote.js
+++
@@ -1,3 +0,0 @@
-var body = $response.body;
-body = '\/*\n@supported C75935FDFB24\n*\/\n' + body;
-$done(body);
--- unblockremote_ipad.js
+++
@@ -1,3 +0,0 @@
-var body = $response.body;
-body = '\/*\n@supported 624A2367ADD0\n*\/\n' + body;
-$done(body);
